e027dee48b777b9d34140451cbc7a96663add843
kyshawn <3200464269@qq.com>
2024-04-12 22:59:14 +0800

    Typing HandWrite

--- 1.LengthOfTuple.ts
+++
@@ -1,7 +0,0 @@
-// 计算元组类型的长度
-
-export type LengthOfTuple<T extends any[]> = T["length"];
-type A = LengthOfTuple<["B", "F", "E"]>; // 3
-type B = LengthOfTuple<[]>; // 0
-
-export {};
--- 5.Push.ts
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-
-export type Push<T extends any[], C> = [...T, C];
-type A = Push<[1, 2, 3], 4>; // [1,2,3,4]
-type B = Push<[1], 2>; // [1, 2]
-export {};
